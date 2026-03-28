7a456546b61c61bdd4aa7758978e0acfb0077b82
Martin Gawron <martin.gawron@proton.me>
2026-03-28 11:45:22 +0100

    Chore: Update organization URL and remotes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://megawron.github.io',
+  site: 'https://0cms-dev.github.io',
   base: '/0cms-example-astro',
 });
